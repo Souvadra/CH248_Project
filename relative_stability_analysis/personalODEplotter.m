9c86bb38b2f3821acc79bfd2eda3479ddc0a97f8
Souvadra <souvadra01@gmail.com>
2021-06-16 18:53:30 +0530

    first round of editing

--- relative_stability_analysis/personalODEplotter.m
+++ relative_stability_analysis/personalODEplotter.m
@@ -1,9 +1,9 @@
 % ODE Solver RACIPE solution files 
 
-path = 'path_to_the_RACIPE_simulations/TEN_SA/2';
+path = '../data/fission_yeast_cc_normal/2';
 components_num = 10;
 external_signal = 0;
-sol_num = 20;
+sol_num = 10;
 
 %% Finding the solution files from the given paths
 [topo_file_info, parameter_names] = parameter_generator(path);
@@ -52,43 +52,43 @@ run_time = 0:1:200; % run time for ode solver
 num_initials = 10; %100;
 
 for ii = 500:520    %size(parameter_sets_for_simulating,1)
-    ga = parameter_sets_for_simulating.("Prod_of_A  ")(ii);
-    gb = parameter_sets_for_simulating.("Prod_of_B  ")(ii);
-    gc = parameter_sets_for_simulating.("Prod_of_C  ")(ii);
-    gd = parameter_sets_for_simulating.("Prod_of_D  ")(ii);
-    ge = parameter_sets_for_simulating.("Prod_of_E  ")(ii);
-    gf = parameter_sets_for_simulating.("Prod_of_F  ")(ii);
-    gg = parameter_sets_for_simulating.("Prod_of_G  ")(ii);
-    gh = parameter_sets_for_simulating.("Prod_of_H  ")(ii);
-    gi = parameter_sets_for_simulating.("Prod_of_I  ")(ii);
-    gj = parameter_sets_for_simulating.("Prod_of_J  ")(ii);
+    ga = parameter_sets_for_simulating.Prod_of_Start(ii);
+    gb = parameter_sets_for_simulating.Prod_of_SK(ii);
+    gc = parameter_sets_for_simulating.Prod_of_Ste9(ii);
+    gd = parameter_sets_for_simulating.Prod_of_Rum1(ii);
+    ge = parameter_sets_for_simulating.Prod_of_Cdc2/Cdc13(ii);
+    gf = parameter_sets_for_simulating.Prod_of_Cdc2/Cdc13*(ii);
+    gg = parameter_sets_for_simulating.Prod_of_Wee1/Mik1(ii);
+    gh = parameter_sets_for_simulating.Prod_of_Cdc25(ii);
+    gi = parameter_sets_for_simulating.Prod_of_PP(ii);
+    gj = parameter_sets_for_simulating.Prod_of_Slp1(ii);
     
-    ka = parameter_sets_for_simulating.("Deg_of_A   ")(ii);
-    kb = parameter_sets_for_simulating.("Deg_of_B   ")(ii);
-    kc = parameter_sets_for_simulating.("Deg_of_C   ")(ii);
-    kd = parameter_sets_for_simulating.("Deg_of_D   ")(ii);
-    ke = parameter_sets_for_simulating.("Deg_of_E   ")(ii);
-    kf = parameter_sets_for_simulating.("Deg_of_F   ")(ii);
-    kg = parameter_sets_for_simulating.("Deg_of_G   ")(ii);
-    kh = parameter_sets_for_simulating.("Deg_of_H   ")(ii);
-    ki = parameter_sets_for_simulating.("Deg_of_I   ")(ii);
-    kj = parameter_sets_for_simulating.("Deg_of_J   ")(ii);
+    ka = parameter_sets_for_simulating.Deg_of_Start(ii);
+    kb = parameter_sets_for_simulating.Deg_of_SK(ii);
+    kc = parameter_sets_for_simulating.Deg_of_Ste9(ii);
+    kd = parameter_sets_for_simulating.Deg_of_Rum1(ii);
+    ke = parameter_sets_for_simulating.Deg_of_Cdc2/Cdc13(ii);
+    kf = parameter_sets_for_simulating.Deg_of_Cdc2/Cdc13*(ii);
+    kg = parameter_sets_for_simulating.Deg_of_Wee1/Mik1(ii);
+    kh = parameter_sets_for_simulating.Deg_of_Cdc25(ii);
+    ki = parameter_sets_for_simulating.Deg_of_PP(ii);
+    kj = parameter_sets_for_simulating.Deg_of_Slp1(ii);
     
    figure
    for jj = 1:num_initials
        [ii,jj]
-       Ajj = (ga/ka)*rand;
-       Bjj = (gb/kb)*rand;
-       Cjj = (gc/kc)*rand;
-       Djj = (gd/kd)*rand;
-       Ejj = (ge/ke)*rand;
-       Fjj = (gf/kf)*rand;
-       Gjj = (gg/kg)*rand;
-       Hjj = (gh/kh)*rand;
-       Ijj = (gi/ki)*rand;
-       Jjj = (gj/kj)*rand;
+       Startjj = (ga/ka)*rand;
+       SKjj = (gb/kb)*rand;
+       Ste9jj = (gc/kc)*rand;
+       Rum1jj = (gd/kd)*rand;
+       Cdc2byCdc13jj = (ge/ke)*rand;
+       Cdc2byCdc13Starjj = (gf/kf)*rand;
+       Wee1byMik1jj = (gg/kg)*rand;
+       Cdc25jj = (gh/kh)*rand;
+       PPjj = (gi/ki)*rand;
+       Slp1jj = (gj/kj)*rand;
 
-       I = [Ajj Bjj Cjj Djj Ejj Fjj Gjj Hjj Ijj Jjj];
+       I = [Startjj SKjj Ste9jj Rum1jj Cdc2byCdc13jj Cdc2byCdc13Starjj Wee1byMik1jj Cdc25jj PPjj Slp1jj];
 
        [t,y] = ode45(@(t,y)dynamic_simulation_TEN_SA(t,y,parameter_sets_for_simulating(ii,:)),run_time,I);
        plot(t,y); hold on;
